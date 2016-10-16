c600ae1c52debc3480224b7ef8f3fa3c12d0f346
alexduf <alexandre.dufournet@gmail.com>
2016-10-17 00:03:46 +0100

    adding packaging script

---
+++ package.sh
@@ -0,0 +1,2 @@
+[ -f gnome-notifications.zip ] && rm gnome-notifications.zip
+zip -r gnome-notifications.zip convenience.js github.svg metadata.json prefs.js schemas extension.js LICENSE README.md stylesheet.css
