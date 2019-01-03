3fc6388cf66e308c2119257dbd1be35636929e1a
Alexandre Dufournet <alex.dufournet@guardian.co.uk>
2019-01-03 16:18:56 +0000

    Implement participation filter (#19)
    * Implement participation filter
    
    * adding prefs
    
    * Bind showBrowserUri to the plugin instance

--- extension.js
+++ extension.js
@@ -28,14 +28,6 @@ function error(message) {
   global.log('[GITHUB NOTIFICATIONS EXTENSION][ERROR] ' + message);
 }
 
-function showBrowserUri() {
-  try {
-    Gtk.show_uri(null, 'https://github.com/notifications', Gtk.get_current_event_time());
-  } catch (e) {
-    error("Cannot open uri " + e)
-  }
-};
-
 const GithubNotifications = new Lang.Class({
   Name: 'GithubNotifications',
 
@@ -53,6 +45,7 @@ const GithubNotifications = new Lang.Class({
   retryIntervals: [60, 120, 240, 480, 960, 1920, 3600],
   hasLazilyInit: false,
   showAlertNotification: false,
+  showParticipatingOnly: false,
   _source: null,
 
   interval: function() {
@@ -100,6 +93,7 @@ const GithubNotifications = new Lang.Class({
     this.hideCount = Settings.get_boolean('hide-notification-count');
     this.refreshInterval = Settings.get_int('refresh-interval');
     this.showAlertNotification = Settings.get_boolean('show-alert');
+    this.showParticipatingOnly = Settings.get_boolean('show-participating-only');
     this.checkVisibility();
   },
 
@@ -138,20 +132,37 @@ const GithubNotifications = new Lang.Class({
 
     this.box.add_actor(icon);
     this.box.add_actor(this.label);
-    this.box.connect('button-press-event', function(actor, event) {
+    this.box.connect('button-press-event', Lang.bind(this, function(actor, event) {
       let button = event.get_button();
 
       if (button == 1) {
-        showBrowserUri();
+        this.showBrowserUri();
       } else if (button == 3) {
         Util.spawn(["gnome-shell-extension-prefs", "github.notifications@alexandre.dufournet.gmail.com"]);
       }
-    });
+    }));
   },
 
 
+  showBrowserUri: function () {
+    try {
+      let url = 'https://github.com/notifications';
+      if (this.showParticipatingOnly) {
+        url = 'https://github.com/notifications/participating';
+      }
+      
+      Gtk.show_uri(null, url, Gtk.get_current_event_time());
+    } catch (e) {
+      error("Cannot open uri " + e)
+    }
+  },
+
   initHttp: function() {
-    this.authUri = new Soup.URI('https://api.github.com/notifications');
+    let url = 'https://api.github.com/notifications';
+    if (this.showParticipatingOnly) {
+      url = 'https://api.github.com/notifications?participating=1';
+    }
+    this.authUri = new Soup.URI(url);
     this.authUri.set_user(this.handle);
     this.authUri.set_password(this.token);
 
